20c3ba8797fb6159285eaf26e93a5f9b240047fb
Joshua Hawxwell <m@hawx.me>
2010-08-20 17:56:11 +0100

    Moved exceptions into separate file so they are loaded before Boolean.

--- lib/clive.rb
+++ lib/clive.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
+require 'clive/exceptions'
 require 'clive/tokens'
 require 'clive/ext'
 require 'clive/option'
@@ -21,47 +22,6 @@ require 'clive/booleans'
 #   c.parse(ARGV)
 #
 class Clive
-
-  # general problem
-  class CliveError < StandardError
-    attr_accessor :args
-    
-    def initialize(*args)
-      @args = args
-    end
-    
-    def self.filter_backtrace(array)
-      unless $DEBUG
-        array = [$0]
-      end
-      array
-    end
-    
-    def set_backtrace(array)
-      super(self.class.filter_backtrace(array))
-    end
-    
-    def message
-      self.reason + ': ' + args.join(' ')
-    end
-    alias_method :to_s, :message
-  
-  end
-  
-  # general problem with input
-  class ParseError < CliveError
-    def reason; "parse error"; end
-  end
-  
-  # a flag has a missing argument
-  class MissingArgument < ParseError
-    def reason; "missing argument"; end
-  end
-  
-  # a option that wasn't defined has been found
-  class InvalidOption < ParseError
-    def reason; "invalid option"; end
-  end
 
   attr_accessor :base
   
