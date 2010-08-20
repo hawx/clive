37aff8a6bf0edc606cc02b641a20bc0a0e2b698c
Joshua Hawxwell <m@hawx.me>
2010-08-20 11:41:58 +0100

    Finished tests for Token

--- test/test_token.rb
+++ test/test_token.rb
@@ -10,8 +10,7 @@ class TestToken < Test::Unit::TestCase
     end
   
     should "convert an array to tokens" do
-      t = Clive::Tokens.new(@array)
-      assert_equal @tokens, t.tokens
+      assert_equal @tokens, Clive::Tokens.to_tokens(@array)
     end
     
     should "convert tokens to an array" do
@@ -25,9 +24,6 @@ class TestToken < Test::Unit::TestCase
       @array = ["add", "-al", "--verbose"]
       @array_split = ["add", "-a", "-l", "--verbose"]
       @tokens = [[:word, "add"], [:short, "a"], [:short, "l"], [:long, "verbose"]]
-      
-      @t = Clive::Tokens.new
-      @t << "add" << "-al" << "--verbose"
     end
     
     should "be created from an array" do
@@ -41,13 +37,30 @@ class TestToken < Test::Unit::TestCase
       assert_equal @tokens, t.tokens
       assert_equal @array_split, t.array
     end
-  
-    should "have tokens" do
-      
+    
+    should "tell whether an array is a token" do
+      t = Clive::Tokens.new
+      assert_equal false, t.token?(["a", "normal", "array"])
+      assert_equal true, t.token?([:word, "help"])
     end
     
-    should "have an array" do
+    should "turn token to string" do
+      t = Clive::Tokens.new
+      assert_equal "-v", t.token_to_string([:short, "v"])
+      assert_equal "--verbose", t.token_to_string([:long, "verbose"])
+      assert_equal "word", t.token_to_string([:word, "word"])
+    end
     
+    should "add new string values" do
+      t = Clive::Tokens.new
+      t << "add" << "-al" << "--verbose"
+      assert_equal @tokens, t.tokens
+    end
+    
+    should "add new token values" do
+      t = Clive::Tokens.new
+      t << [:word, "add"] << [:short, "a"] << [:short, "l"] << [:long, "verbose"]
+      assert_equal @array_split, t.array
     end
   
   end
