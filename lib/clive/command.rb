a9997aa2c7c074a015d09ed319613f2878a19f16
Joshua Hawxwell <m@hawx.me>
2011-06-20 14:47:49 +0100

    Wrote basic implementation of Command

--- lib/clive/command.rb
+++ lib/clive/command.rb
@@ -1,6 +1,48 @@
 module Clive
 
-  class Command
+  class Command < Option
+  
+    def initialize(names, desc="", opts={}, &block)
+      @names = names
+      @desc  = desc
+      @opts  = opts
+      @block = block
+      
+      @options = []
+      current_desc
+    end
+    
+    def current_desc
+      r = @_last_desc
+      @_last_desc = ""
+      r
+    end
+    
+    def option(*args, &block)
+      s, l, d, o = nil, nil, current_desc, {}
+      args.each do |i|
+        case i
+          when Symbol then i.size > 1 ? l = i : s = i
+          when String then d = i
+          when Hash   then o = i
+        end
+      end
+      @options << Option.new(s, l, d, o, &block)
+    end
+    alias_method :opt, :option
+    
+    def description(arg)
+      @_last_desc = arg
+    end
+    alias_method :desc, :description
+    
+    def has_option?(arg)
+      @options.any? {|i| i.short == arg || i.long == arg }
+    end
+    
+    def [](arg)
+      @options.find {|i| i.short == arg || i.long == arg }
+    end
   
   end
 end
--- test/test_command.rb
+++ test/test_command.rb
@@ -2,4 +2,27 @@ require_relative 'helper'
 
 class TestCommand < MiniTest::Unit::TestCase
 
+  def setup
+    @command = Clive::Command.new([:name], "A description", {})
+  end
+
+  def test_dsl
+    @command.option :force, 'Forces building'
+    assert @command.has_option?(:force)
+    assert_equal 'Forces building', @command[:force].desc
+    
+    @command.desc 'The description for the next option'
+    @command.opt :n, :next
+    assert @command.has_option?(:n)
+    assert @command.has_option?(:next)
+    assert_equal 'The description for the next option', @command[:next].desc
+  end
+  
+  def test_running
+    a = nil
+    c = Clive::Command.new([:name], "A description", {}) { a = 5 }
+    c.run
+    assert_equal 5, a
+  end
+
 end
