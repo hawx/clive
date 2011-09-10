b64ebf1ec210d1ac9a383cf7b65ae85c3abc2a80
Joshua Hawxwell <m@hawx.me>
2011-09-10 11:25:51 +0100

    Added more tests for CliveTest.
    - checks options with underscores can be called with dashes.
    - tests complex argument lists (FAILS)

--- test/clive/clive_test.rb
+++ test/clive/clive_test.rb
@@ -5,6 +5,8 @@ require 'helper'
 class CliveTestClass
   extend Clive
   
+  header 'Usage: clive_test.rb [command] [options]'
+  
   opt :version, tail: true do
     puts "Version 1"
   end
@@ -13,10 +15,15 @@ class CliveTestClass
   opt :v, :verbose, as: Boolean
   
   opt :s, :size, 'Size of thing', arg: '<size>', as: Float
-  
+  opt :S, :super_size
+    
   desc 'Print <message> <n> times'
   opt :print, arg: '<message> <n>', as: [String, Integer] do
     n.times { puts message }
+  end
+  
+  opt :complex, :arg => '[<one>] <two> [<three>]', :match => [ /\d/, /\d\d/, /\d\d\d/ ] do |a,b,c|
+    puts "a: #{a}, b: #{b}, c: #{c}"
   end
   
   command :new, 'Creates new things', arg: '<dir>' do
@@ -37,7 +44,6 @@ class CliveTestClass
 end
 
 
-
 class TestClive < MiniTest::Unit::TestCase
 
   def test_boolean_switches
@@ -54,11 +60,69 @@ class TestClive < MiniTest::Unit::TestCase
     end
   end
   
+  def test_calling_with_long_names
+    a,s = CliveTestClass.run %w(--super-size)
+    assert_equal({:super_size => true}, s)
+  end
+  
   def test_commands
     $stdout.expect(:puts, nil, ["Creating post in ~/my_site"])
     a,s = CliveTestClass.run %w(-v new --type post ~/my_site --no-auto arg)
     assert_equal %w(arg), a
     assert_equal({:verbose => true, :new => {:type => :post}, :auto => false}, s)
+    $stdout.verify
+  end
+  
+  def test_complex_arguments
+    $stdout.expect(:puts, nil, ["a: 1, b: 22, c: 333"])
+    a,s = CliveTestClass.run %w(--complex 1 22 333)
+    $stdout.verify
+    $stdout.reset
+    
+    $stdout.expect(:puts, nil, ["a: 1, b: 22, c: "])
+    CliveTestClass.run %w(--complex 1 22)
+    $stdout.verify
+    $stdout.reset
+    
+    $stdout.expect(:puts, nil, ["a: , b: 22, c: 333"])
+    CliveTestClass.run %w(--complex 22 333)
+    $stdout.verify
+    $stdout.reset
+    
+    $stdout.expect(:puts, nil, ["a: , b: 22, c: "])
+    CliveTestClass.run %w(--complex 22)
+    $stdout.verify
+    $stdout.reset
+    
+    assert_raises Clive::Parser::MissingArgumentError do
+      CliveTestClass.run %w(--complex 1)
+    end
+    
+    assert_raises Clive::Parser::MissingArgumentError do
+      CliveTestClass.run %w(--complex 333)
+    end
+  end
+  
+  def test_displays_help
+    $stdout.expect(:puts, nil, [<<EOS])
+Usage: clive_test.rb [command] [options]
+
+  Commands:
+    new <dir>                            # Creates new things
+    help [<command>]                     # Display help
+
+  Options:
+    -a, --[no-]auto
+    --complex [<one>] <two> [<three>]
+    --print <message> <n>                # Print <message> <n> times
+    -s, --size <size>                    # Size of thing
+    -S, --super-size
+    -v, --[no-]verbose
+    -h, --help                           # Display this help message
+    --version
+EOS
+    
+    a,s = CliveTestClass.run %w(help)
     $stdout.verify
   end
   
