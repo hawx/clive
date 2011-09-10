76a15835914926d57c6e98b30e4e4687435cb44e
Joshua Hawxwell <m@hawx.me>
2011-09-10 16:20:27 +0100

    Removed all of the Argument parsing code from Option

--- lib/clive/option.rb
+++ lib/clive/option.rb
@@ -120,156 +120,14 @@ module Clive
       names.last
     end
 
-    # Pads +obj+ with +pad+ until it has size of +max+
-    # @param obj [#size, #<<]
-    # @param max [Integer]
-    # @param pad [Object]
-    def pad(obj, max, pad=nil)
-      if obj.size < max
-        (max - obj.size).times { obj << pad }
-      end
-      obj
-    end
-    
     # @return [Array<Hash,Array>]
     #  The first is a hash containing options for the Option.
     #  The second is an array of arguments the option takes.
     def do_options(opts)
-      opts, hash = sort_opts(opts)
-      args = infer_args(args_to_hash(hash))
-
-      args.map! {|arg| Argument.new(arg[:name] || 'arg', arg.without(:name)) }
-      
-      [opts, args]
+      a = ArgumentParser.new(opts)
+      return a.opts, a.args
     end
     
-    # This does two things, it splits the options hash into two hashes as explained
-    # below. AND normalises the names of the hash keys meaning they can be accessed
-    # easily.
-    #
-    # @return [Array[Hash]] Two hashes.
-    #  The first hash contains all options relevant to the Option, to be stored
-    #  in +@opts+.
-    #  The second hash contains all options for building the arguments and should
-    #  be used in the relevant methods.
-    #  Hash keys for the returned hashes will be mapped to standard names from
-    #  common variations.
-    def sort_opts(hash)
-      arg_keys = {
-        :args        => [:arg],
-        :types       => [:type, :kind, :as],
-        :matches     => [:match],
-        :withins     => [:within, :in],
-        :defaults    => [:default],
-        :constraints => [:constraint]
-      }.flip
-
-      opt_keys = {
-        :head => [:head],
-        :tail => [:tail]
-      }.flip
-
-      arg, opt = {}, {}
-      hash.each do |k, v|
-        if arg_keys.has_key?(k)
-          arg[arg_keys[k]] = v
-        elsif opt_keys.has_key?(k)
-          opt[opt_keys[k]] = v
-        elsif arg_keys.has_value?(k)
-          arg[k] = v
-        elsif opt_keys.has_value?(k)
-          opt[k] = v
-        end
-      end
-
-      [opt, arg]
-    end
-
-    # This turns the arguments string and other options into a nicely formatted 
-    # hash.
-    def args_to_hash(opts)
-      withins = []
-      # Normalise withins separately as it will usually be an Array.
-      if opts.has_key?(:withins)
-        if opts[:withins].respond_to?(:[]) && opts[:withins][0].is_a?(Array)
-          withins = opts[:withins]
-        else
-          withins = [opts[:withins]]
-        end
-      end
-      
-      # Make everything an Array
-      multiple = {
-        :types       => Array(opts[:types])       || [],
-        :matches     => Array(opts[:matches])     || [],
-        :withins     => withins,
-        :defaults    => Array(opts[:defaults])    || [],
-        :constraints => Array(opts[:constraints]) || []
-      }
-      
-      # Find the largest Array...
-      max = multiple.values.map(&:size).max
-      
-      singles = []
-      # Split into an array of hashes, with each hash for each item of the previous
-      # Arrays.
-      # ie. go from {:as => [b, c], ...}
-      #          to [{:a => b, ...}, {:a => c, ...}, ...]
-      #
-      max.times do |i|
-        singles[i] = {}
-        {
-          :types => :type,
-          :matches => :match,
-          :withins => :within,
-          :defaults => :default,
-          :constraints => :constraint
-        }.each do |plural, single|
-          singles[i][single] = multiple[plural][i] if multiple[plural][i]
-        end
-      end
-      
-      # If no arg string to parse return now.
-      return singles unless opts[:args]
-
-      optional = false
-      cancelled_optional = false
-      # Parse the argument string and merge in previous options from +singles+.
-      opts[:args].split(' ').zip(singles).map {|arg, opts|
-        if cancelled_optional
-          optional = false
-          cancelled_optional = false
-        end
-
-        cancelled_optional = true if arg[-1] == ']'
-
-        if arg[0] == '['
-          optional = true
-        elsif arg[0] != '<'
-          raise InvalidArgumentString
-        end
-        
-        {:name => arg.gsub(/^\[?\<|\>\]?$/, ''), :optional => optional}.merge(opts || {})
-      }
-    end
-    
-    # Infer arguments that haven't been explicitly defined by name. This allows you
-    # to just say "it" should be within the range +1..5+ and have an argument 
-    # created without having to pass +:arg => '<choice>'+.
-    def infer_args(opts)
-      opts.map do |hash|
-        if hash.has_key?(:name)
-          hash
-        else
-          if hash.has_any_key?(:type, :match, :constraint, :within, :default)
-            hash.merge!({:name => 'arg'})
-          end
-          hash.merge!({:optional => true}) if hash.has_key?(:default) && !hash.has_key?(:optional)
-        
-          hash
-        end
-      end
-    end
 
     # @return [String]
     def to_s
@@ -396,7 +254,7 @@ module Clive
     def before_help_string
       b = to_s
       if @args != [] && !boolean?
-        b << " " << @args.map {|i| i.to_s }.join(' ').gsub('] [', ' ')
+        b << " " << @args.to_s
       end
       b
     end
