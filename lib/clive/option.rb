5b5acadb5ec0f5fb8f46f46bb28ec55c281b3583
Joshua Hawxwell <m@hawx.me>
2011-09-10 15:26:32 +0100

    Move Option::RunClass out to option/runner

--- lib/clive/option.rb
+++ lib/clive/option.rb
@@ -427,38 +427,4 @@ module Clive
     
   end
 
-  # Class for running in
-  class RunClass
-    class << self
-
-      def _run(args, state, fn)
-        @args = args
-        @state = state
-        return unless fn
-        if fn.arity > 0
-          instance_exec(*args.values, &fn)
-        else
-          instance_exec(&fn)
-        end
-      end
-
-      def get(key)
-        @state[key]
-      end
-
-      def set(key, value)
-        @state[key] = value
-      end
-
-      def method_missing(sym, *args)
-        if @args.has_key?(sym)
-          @args[sym]
-        else
-          super
-        end
-      end
-
-    end
-  end
-
 end
