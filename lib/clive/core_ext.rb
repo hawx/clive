85f07808e595751ad9b80f04ae4dc3c32ffabdb1
Joshua Hawxwell <m@hawx.me>
2011-09-10 11:17:10 +0100

    Added more evil monkey patches

--- lib/clive/core_ext.rb
+++ lib/clive/core_ext.rb
@@ -39,4 +39,62 @@ class Hash
     end
     res
   end
+  
+end
+
+class Array
+  
+  # @example
+  #
+  #   [1, 2, 3].zip_to_pattern(%w(a c), [true, false, true])
+  #   #=> [[1, 'a'], [2, nil], [3, 'c']]
+  #
+  def zip_to_pattern(other, pattern)
+    other = other.dup
+    diff = other.size - pattern.find_all {|i| i == true }.size
+    
+    result = dup.map {|i| [i, nil] }
+    pattern.each_index do |i|
+      curr = pattern[i]
+      if (curr.respond_to?(:call) && curr.call(other.first)) || curr
+        result[i][1]= other.shift
+      elsif diff > 0
+        diff -= 1
+        result[i][1] = other.shift
+      end
+    end
+    
+    result
+  end
+  
+end
+
+class Symbol
+
+  # A valid Symbol can not contain dashes, so instead underscores are used,
+  # this method returns a String with all underscores changed to dashes.
+  #
+  # @example
+  #   :a_b_c.dashify #=> 'a-b-c'
+  #
+  # @see String#symbolify
+  # @return [String]
+  def dashify
+    to_s.gsub('_', '-')
+  end
+end
+
+class String
+
+  # The string will probably contain dashes as they look nicer, but to be a 
+  # Symbol these must be removed and replaced with underscores.
+  #
+  # @example
+  #   'a-b-c'.symbolify #=> :a_b_c
+  #
+  # @see Symbol#dashify
+  # @return [Symbol]
+  def symbolify
+    gsub('-', '_').to_sym
+  end
 end
