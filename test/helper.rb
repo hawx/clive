a0ca29e057177b45b932aa32527c86e2a09e714d
Joshua Hawxwell <m@hawx.me>
2011-09-22 16:00:50 +0100

    Dependencies updated. Tests now run when duvet can't be loaded

--- test/helper.rb
+++ test/helper.rb
@@ -2,8 +2,12 @@ $: << File.dirname(__FILE__) + '/..'
 
 if RUBY_VERSION >= "1.9"
   require 'fileutils'
-  require 'duvet'
-  Duvet.start :filter => 'lib/clive'
+  begin
+    require 'duvet'
+    Duvet.start :filter => 'lib/clive'
+  rescue LoadError
+    # it doesn't really matter
+  end
 end
 
 require 'lib/clive'
