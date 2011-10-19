1e59ea08fd6ccf67e8a14e05386449af5b75d251
Joshua Hawxwell <m@hawx.me>
2011-10-19 18:28:35 +0100

    Another unit -> spec

--- test/clive/formatter_test.rb
+++ test/clive/formatter_test.rb
@@ -1,35 +1,38 @@
 $: << File.dirname(__FILE__) + '/..'
 require 'helper'
 
+describe Clive::Formatter do
 
-class CliveTestClass2
-  extend Clive
-  
-  header 'Usage: clive_test.rb [command] [options]'
-  footer 'Further help is available online'
-  
-  opt :version, :tail => true
-  
-  opt :S, :super_size, :head => true
-  bool :a, :auto
-  opt :s, :size, 'Size of thing', :arg => '<size>'
-  
-  
-  desc 'A super long description for a super stupid option, this should test the _extreme_ wrapping abilities as it should all be aligned. Maybe I should go for another couple of lines just for good measure. That\'s all'
-  opt :complex, :arg => '[<one>] <two> [<three>]'
-  
-  command :new, 'Creates new things', :arg => '<dir>' do
-    opt :type, :in => %w(post page blog), :default => :page
-    opt :force, 'Force overwrite'
+  let :clive do
+    Class.new {
+      extend Clive
+      
+      header 'Usage: clive_test.rb [command] [options]'
+      footer 'Further help is available online'
+      
+      opt :version, :tail => true
+      
+      opt :S, :super_size, :head => true
+      bool :a, :auto
+      opt :s, :size, 'Size of thing', :arg => '<size>'
+      
+      
+      desc 'A super long description for a super stupid option, this should test the _extreme_ wrapping abilities as it should all be aligned. Maybe I should go for another couple of lines just for good measure. That\'s all'
+      opt :complex, :arg => '[<one>] <two> [<three>]'
+      
+      command :new, 'Creates new things', :arg => '<dir>' do
+        opt :type, :in => %w(post page blog), :default => :page
+        opt :force, 'Force overwrite'
+      end
+    }
   end
-
-end
-
-
-class FormatterTest < MiniTest::Unit::TestCase
-
-  def test_builds_help_string
-    r = <<EOS
+  
+  describe '#to_s' do
+    
+    it 'builds the help string' do
+      this {
+        clive.run s('help'), :formatter => Clive::Formatter.new(80)
+      }.must_output <<EOS
 Usage: clive_test.rb [command] [options]
 
   Commands:
@@ -52,16 +55,12 @@ Usage: clive_test.rb [command] [options]
 
 Further help is available online
 EOS
-
-    assert_output r do
-      CliveTestClass2.run s('help'), :formatter => Clive::Formatter.new(80)
     end
-  end
-  
-  def test_help_string_obeys_minimum_ratio
-#------------------------------------------------------------------------------| 80
-#                                                       |======================| 24
-    r = <<EOS
+    
+    it 'obeys minimum ratio' do
+      this {
+        clive.run s('help'), :formatter => Clive::Formatter.new(80, 2, 0.7)
+      }.must_output r = <<EOS
 Usage: clive_test.rb [command] [options]
 
   Commands:
@@ -92,17 +91,14 @@ Further help is available online
 EOS
 #------------------------------------------------------------------------------| 80
 #                                                       |======================| 24
-    
-    assert_output r do
-      CliveTestClass2.run s('help'), :formatter => Clive::Formatter.new(80, 2, 0.7)
+            
+            
     end
-  end
-  
-  def test_help_string_obeys_maximum_ratio
-  
-#------------------------------------------------------------------------------| 80
-#======================|24#
-    r = <<EOS
+    
+    it 'obeys the maximum ratio' do
+      this {
+        clive.run s('help'), :formatter => Clive::Formatter.new(80, 2, 0, 0.3)
+      }.must_output <<EOS
 Usage: clive_test.rb [command] [options]
 
   Commands:
@@ -127,10 +123,7 @@ Further help is available online
 EOS
 #======================|  #
 #------------------------------------------------------------------------------|
-
-    assert_output r do
-      CliveTestClass2.run s('help'), :formatter => Clive::Formatter.new(80, 2, 0, 0.3)
     end
+    
   end
-
 end
