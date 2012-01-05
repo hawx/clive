36917f37634e69e491ee19f746fdc9d28521efd2
Joshua Hawxwell <m@hawx.me>
2012-01-05 17:59:12 +0000

    Removed Symbol#<=> patch and moved #Clive to Kernel#Clive

--- lib/clive.rb
+++ lib/clive.rb
@@ -1,15 +1,5 @@
 $: << File.dirname(__FILE__)
 
-# Ruby 1.8.7 and others
-unless :a_symbol.respond_to?(:<=>)
-  class Symbol
-    def <=>(other)
-      self.to_s <=> other.to_s
-    end
-  end
-end
-
-
 require 'clive/error'
 require 'clive/output'
 require 'clive/version'
@@ -29,10 +19,8 @@ require 'clive/parser'
 require 'clive/base'
 
 
-# Clive is a DSL for creating command line interfaces. Extend a class with it
-# to use.
-#
-# @example
+# Clive is a DSL for creating command line interfaces. Generally to use it you
+# will inherit from it with your own class.
 #
 #   class CLI < Clive
 #     opt :working, 'Test if it is working' do
@@ -45,26 +33,36 @@ require 'clive/base'
 #   # app.rb --working
 #   #=> "YEP!"
 #
+# But it is possible to create a new instance of Clive instead in almost the
+# same way.
+#
+#   cli = Clive.new do
+#     opt :working, 'Test if it is working' do
+#       puts "YEP!".green
+#     end
+#  end
+#
+#  cli.run ARGV
+#
+# For very small tasks where you _just_ need to collect options passed and query
+# about them you can use {Kernel#Clive}.
+#
+#   r = Clive(:working).run(ARGV)
+#   if r.working
+#     puts "YEP!".green
+#   end
+#
 class Clive
 
   extend Type::Lookup
-  
-  # @group Class style
-  #
-  # class CLI < Clive
-  #   opt :v, :verbose
-  # end
-  #
-  # r = CLI.run ARGV
-  #
-  
+
   class << self
     attr_accessor :instance
-    
+
     # Sets up proxy methods for each relevent method in {Base} to an instance of {Base}.
     def inherited(klass)
       klass.instance = Base.new
-      
+
       str = (Base.instance_methods(false) | Command.instance_methods(false)).map do |sym|
         <<-EOS
           def self.#{sym}(*args, &block)
@@ -74,52 +72,63 @@ class Clive
       end.join("\n")
       klass.instance_eval str
     end
-    
+
     def method_missing(sym, *args, &block)
       instance.send(sym, *args, &block)
     end
-    
-    def respond_to_missing?(sym, include_private)
-      instance.respond_to?(sym, include_private)
+
+    if Kernel.respond_to?(:respond_to_missing?)
+      def respond_to_missing?(sym, include_private=false)
+        instance.respond_to?(sym, include_private)
+      end
+    else
+      def respond_to?(sym, include_private=false)
+        instance.respond_to?(sym, include_private)
+      end
     end
   end
-  
-  # @group Instance style
+
+  # This allows you to use Clive without defining a class, but while keeping all
+  # of the control.
   #
-  # c = Clive.new { opt :v, :verbose }
-  # r = c.run ARGV
-  # 
-  
-  # Proxy for Clive::Base.new
+  # @example
+  #
+  #   c = Clive.new { opt :v, :verbose }
+  #   r = c.run ARGV
+  #
   def self.new(&block)
     Base.new(&block)
   end
 
 end
 
-# The quickest way to grab a few options. This form does not allow arguments or 
-# commands! It is meant to be quick and simple.
-#
-# @example
-#
-#   r = Clive(:verbose, [:b, :bare]).run(%w(--verbose))
-#   r.bare     #=> false
-#   r.verbose  #=> true
-#
-#
-#   # The above example is equivalent to 
-#   r = Clive.new {
-#     opt :verbose
-#     opt :b, :bare
-#   }.run(%w(--verbose))
-#
-# @param names [#to_sym, Array<#to_sym>] List of names to create options for
-# @return [Clive] A clive instance setup with the correct options
-#
-def Clive(*names)
-  c = Clive::Base.new
-  names.each do |o|
-    c.option *Array(o).map(&:to_sym)
+module Kernel
+
+  # The quickest way to grab a few options. This form does not allow arguments or
+  # commands! It is meant to be quick and simple.
+  #
+  # @example
+  #
+  #   r = Clive(:verbose, [:b, :bare]).run(%w(--verbose))
+  #   r.bare     #=> false
+  #   r.verbose  #=> true
+  #
+  #
+  #   # The above example is equivalent to
+  #   r = Clive.new {
+  #     opt :verbose
+  #     opt :b, :bare
+  #   }.run(%w(--verbose))
+  #
+  # @param names [#to_sym, Array<#to_sym>] List of names to create options for
+  # @return [Clive] A clive instance setup with the correct options
+  #
+  def Clive(*names)
+    c = Clive::Base.new
+    names.each do |o|
+      c.option *Array(o).map(&:to_sym)
+    end
+    c
   end
-  c
+
 end
