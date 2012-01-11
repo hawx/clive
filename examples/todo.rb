62cb7c08dcb8bc207a5d7b8aa8bc455926b4ca0c
Joshua Hawxwell <m@hawx.me>
2012-01-11 16:27:14 +0000

    Removed help command from todo example

--- examples/todo.rb
+++ examples/todo.rb
@@ -3,23 +3,23 @@ require_relative '../lib/clive'
 TODO_FILE = File.expand_path('~/Desktop/todos.txt')
 
 class Todo < Clive
-  
+
   def self.write(mode, text)
     File.open(TODO_FILE, mode) {|f| f.write(text) }
   end
-  
+
   def self.read
     r = IO.read(TODO_FILE) rescue 'No tasks'
     r = 'No tasks' if r.empty?
     r
   end
-  
-  
+
+
   desc 'List all todo items'
   opt :l, :list do
     puts Todo.read
   end
-  
+
   desc 'Delete the item with index given'
   opt :d, :delete, :arg => '<index>', :as => Integer do
     lines = Todo.read.split("\n")
@@ -30,7 +30,7 @@ class Todo < Clive
       Todo.write 'w', lines.join("\n")
     end
   end
-  
+
   desc 'Add item to list'
   opt :a, :add, :arg => '<task>' do
     Todo.write 'a+', "- #{task}\n"
@@ -38,4 +38,4 @@ class Todo < Clive
 
 end
 
-Todo.run ARGV
+Todo.run ARGV, :help_command => false
