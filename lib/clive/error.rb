4d702355a5e460813abb8cd3ec0c631501d41e38
Joshua Hawxwell <m@hawx.me>
2012-06-22 19:54:48 +0100

    Define KeyError for 1.8.7

--- lib/clive/error.rb
+++ lib/clive/error.rb
@@ -1,8 +1,12 @@
+unless defined?(KeyError)
+  class KeyError < IndexError; end
+end
+
 class Clive
 
-  # For general errors with Clive. It stripts most of the backtrace which 
+  # For general errors with Clive. It stripts most of the backtrace which
   # you don't really want, and allows you to set nice error messages
-  # using {.reason}. Arguments can be passed and then used in messages by 
+  # using {.reason}. Arguments can be passed and then used in messages by
   # referencing with +#n+ tokens, where +n+ is the index of the argument.
   #
   # A lot of this is pulled from OptionParser::ParseError see
@@ -18,13 +22,13 @@ class Clive
   #
   class Error < StandardError
     attr_accessor :args
-    
+
     # @param args
     #   Arguments that can be accessed with '#n' in {.reason}.
     def initialize(*args)
       @args = args
     end
-    
+
     # Removes all references to files which are not the file being run
     # unless in $DEBUG mode.
     def self.filter_backtrace(array)
@@ -33,22 +37,22 @@ class Clive
       end
       array
     end
-    
+
     # Set the reason for the error class.
     # @param str [String]
     def self.reason(str)
       @reason = str
     end
-    
+
     # Accessor for the reason set with {.reason}.
     def self._reason
       @reason
     end
-    
+
     def set_backtrace(array)
       super(self.class.filter_backtrace(array))
     end
-    
+
     # Build the message by substituting the arguments into the reason.
     def message
       self.class._reason.gsub(/#\d/) do |i|
@@ -61,6 +65,6 @@ class Clive
       end
     end
     alias_method :to_s, :message
-    
+
   end
 end
